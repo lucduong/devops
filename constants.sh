db4ea7c60d87d946d9bd1dabdc2a3742affcf2f7
Luc <luc@ltv.vn>
2018-04-20 06:37:34 +0700

    Update base install

--- constants.sh
+++ constants.sh
@@ -3,9 +3,9 @@
 # This is script which defines constants
 # -------
 
-export DEVOPS_HOME=/home/devops
+export DEVOPS_HOME=/root/devops
 export CATALINA_HOME=$DEVOPS_HOME/tomcat
-export BASE_INSTALL=/home/ubuntu/devops
+export BASE_INSTALL=/root/devops/devops
 export TMP_INSTALL=/tmp/devops-install
 export NGINX_CONF=$BASE_INSTALL/_ubuntu/etc/nginx
 export LOCALESUPPORT=en_US.utf8
